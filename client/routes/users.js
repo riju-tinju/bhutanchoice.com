8b6c5f08e637f4378360e12f3878d8b3750c0d89
riju <riju.tinju@gmail.com>
2025-08-23 12:13:39 +0530

    time checking page added

--- client/routes/users.js
+++ client/routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment-timezone');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/check-time', (req, res) => {
+
+  try {
+    const normalTimeFromBackend = new Date();
+    const bhutanTime = moment().tz('Asia/Thimphu');
+    const indianTime = moment().tz('Asia/Kolkata');
+    const dubaiTime = moment().tz('Asia/Dubai');
+    const utcTime = moment().utc();
+
+    const timeObj = {
+      normalTimeFromBackend,
+      bhutanTime: bhutanTime.format(),
+      indianTime: indianTime.format(),
+      dubaiTime: dubaiTime.format(),
+      utcTime: utcTime.format()
+    }
+    res.render('checkTime', { timeObj })
+  } catch (err) {
+    console.error("Error in /check-time route:", err);
+  }
+
+})
 
 module.exports = router;
